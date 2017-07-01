a8f5319ff4b3273ae02421b421f9bc72cafbf8dd
dmbdesignpdx <dmblakedesign@gmail.com>
2017-06-30 18:57:51 -0700

    initital commit

---
+++ README.md
@@ -0,0 +1,32 @@
+#### Hello, I am a front-end web designer living in Portland, Oregon.
+
+### Design
+
+- InDesign
+- Illustrator
+- XD
+- Photoshop
+- After Effects
+- Premiere Pro
+
+### Development
+
+- HTML5
+- CSS3
+- JavaScript (+ES2015)
+- SCSS (+SassScript)
+- Git
+- CoffeeScript
+- Markdown
+- Jekyll
+- XML
+- YAML
+- Node.js
+
+### Experience
+
+- Branding
+- Responsive Web Design
+- UX/UI Design
+- Art Direction
+- Accessibility
---
+++ src/main.js
@@ -0,0 +1,92 @@
+// -- Vars -- //
+
+const
+page = cls => cls === document.body.className,
+wh = () => window.innerHeight * 0.33,
+icon = document.querySelector("body > header svg"),
+nav = document.querySelector("#nav")
+
+let
+check = 0,
+original = window.pageYOffset,
+max = 0,
+bounce
+
+
+
+// -- Functions -- //
+
+// Arrow Fade on Scroll
+
+function arrowFade() {
+	if (window.scrollY > wh() && !check) {
+		icon.classList.add("fade")
+		check = 1
+	} else if (window.scrollY < wh() && check) {
+		icon.classList.remove("fade")
+		check = 0
+	}
+}
+
+
+// Nav Scroll Action
+
+function scroll() {
+	const update = window.pageYOffset,
+	nh = parseInt(window.getComputedStyle(nav).height)
+
+	// Check scroll position
+	if (window.pageYOffset > nh) {
+		nav.classList.add("fixed")
+		nav.classList.add("time")
+
+		// Scroll bounce check
+		if (original > -1 && max > update) {
+
+			// Show or hide nav based on all checks
+			if (update > original && !bounce) {
+				nav.classList.remove("show")	
+				bounce = 1
+			} else if (original >= update) {
+				nav.classList.add("show")
+				bounce = 0
+			}
+
+		}
+
+	// Reset
+	} else if (0 === window.pageYOffset) {
+		nav.classList.remove("fixed")
+		nav.classList.remove("show")	
+	} else {
+		nav.classList.remove("time")
+	}
+
+	// Update scroll position
+	original = update
+}
+
+
+// Check Window Size
+
+function size() {
+	const full = document.documentElement.scrollHeight,
+	height = window.innerHeight
+	max = full - height
+}
+
+
+
+// -- Invoke -- //
+
+window.addEventListener("load", function ready() {
+	window.removeEventListener("load", ready)
+	window.addEventListener("resize", size)
+	window.addEventListener("scroll", () => {
+		scroll()
+		arrowFade()
+	})
+	
+	scroll()
+	size()
+})
