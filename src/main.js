0f197cd99aaf5174f4b25af1e7e226cbe08a1532
dmbdesignpdx <daniel@danielblake.design>
2018-10-09 12:46:33 -0700

    housekeeping

--- src/main.js
+++ src/main.js
@@ -1,44 +1,50 @@
-var SmoothScroll = require("./smooth-scroll")
+"use strict"
 
-function $(el) {
-   el = "window" === el ? [window] : "body" === el ? [document.body] : "html" === el ? [document.documentElement] : "head" === el ? [document.head] : [].slice.call(document.querySelectorAll(el))
-   return 1 < el.length ? el : el[0]
-}
+// === Imports === //
 
-function addTo(el, evt, func, opt = null) {
-   !Array.isArray($(el)) && (el = [$(el)])
-   el.map(elm => {elm.addEventListener(evt, func, opt)})
-}
+// Vendor
+var SmoothScroll = require("./plugins/smooth-scroll")
 
-function rmvFrom(el, evt, func, opt = null) {
-   !Array.isArray($(el)) && (el = [$(el)])
-   el.map(elm => {elm.removeEventListener(evt, func, opt)})
-}
+// Ours
+import { $, addTo, rmvFrom } from "./plugins/select"
+import Nav from "./nav"
 
-// -- Vars -- //
+
+
+// === Vars === //
 
 var
 icon = $("body > header svg"),
 nav = $("#nav"),
 scroll = SmoothScroll('a[href*="#"]'),
-original = window.pageYOffset,
-check = 0,
-max = 0,
-bounce
+check = 0
 
 
 
-// -- Functions -- //
+// === Functions === //
 
-// Page
+// -- Which Page
 
 function page(cls) {
 	return cls === $("body").className
 }
 
 
+// -- Hero Underline
 
-// Arrow Fade on Scroll
+function underline() {
+   var span = $("h1 span")
+   if (Array.isArray(span)) {
+      span.map(function spanMap(item) {
+         item.classList.add("js-underline")
+      })
+   } else {
+      span.classList.add("js-underline")
+   }
+}
+
+
+// -- Arrow Fade
 
 function arrowFade() {
    var wh = Math.round(window.innerHeight * 0.33),
@@ -54,53 +60,7 @@ function arrowFade() {
 }
 
 
-// Nav Scroll Action
-
-function navScroll() {
-	var update = window.pageYOffset,
-	nh = parseInt(window.getComputedStyle(nav).height)
-
-	// Check scroll position
-	if (window.pageYOffset > nh) {
-		nav.classList.add("fixed")
-		nav.classList.add("time")
-
-		// Scroll bounce check
-		if (original > -1 && max > update) {
-
-			// Show or hide nav based on all checks
-			if (update > original && !bounce) {
-				nav.classList.remove("show")	
-				bounce = 1
-			} else if (original > (update + 2)) {
-				nav.classList.add("show")
-				bounce = 0
-			}
-
-		}
-
-	// Reset
-	} else if (0 === window.pageYOffset) {
-		nav.classList.remove("fixed")
-		nav.classList.remove("show")	
-	} else {
-		nav.classList.remove("time")
-	}
-
-	// Update scroll position
-	original = update
-}
-
-
-// Check Window Size
-
-function size() {
-	const full = $("html").scrollHeight,
-	height = window.innerHeight
-	max = full - height
-}
-
-
+// -- Form Validation
 
 function validate() {
    document.drop.classList.add("sub")
@@ -108,29 +68,18 @@ function validate() {
 
 
 
-// -- Invoke -- //
+// === Invoke === //
 
-if (page("home")) {
-   document.drop.send.addEventListener("click", validate)
-}
-
-addTo("window", "load", function ready() {
-	addTo("window", "resize", size)
+addTo("window", "load", function siteReady() {
 	addTo("window", "scroll", () => {
-		navScroll()
 		arrowFade()
 	})
 
-   // Hero underline animations
-   var span = $("h1 span")
-	if (Array.isArray(span)) {
-      span.map(item => {
-         item.classList.add("underline")
-      })
-   } else {
-      span.classList.add("underline")
+   underline()
+
+   if (page("home")) {
+      document.drop.send.addEventListener("click", validate)
    }
-	
-	navScroll()
-   size()
 }, {once: true})
+
+Nav()
