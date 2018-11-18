4b71a100d89b33353e41c7b601ac8f0c2083014d
dmbdesignpdx <daniel@danielblake.design>
2018-11-17 21:12:22 -0800

    updates scripts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /**
- *  @file MainJS
- *  @description Main script for danielblake.design
- *
+ *  @file Main
+ *  @description Root script for danielblake.design
  */
+
 
 
 // Mark
@@ -28,7 +28,7 @@ import navScroll from "./nav"
 
 const icon = document.querySelector("body > header svg")
 
-let check = 0
+let check
 
 
 
@@ -39,78 +39,61 @@ let check = 0
 
 /**
  *  @name page
- *
  *  @description Checks which page is being viewed
- *  @param {string} cls - class name to check
- *  @returns {boolean} Yea or Nay
+ *  @param {string} cls - class name
+ *  @returns {boolean}
  *
  */
 function page(cls) {
-
    return cls === document.body.className
-
 }
 
 
 /**
  *  @name underline
- *
- *  @description Draws a line under the span(s) in the h1
- *  @returns {undefined} Adds the class "js-underline"
+ *  @description Adds the class "js-underline" to the h1 span
+ *  @returns {undefined}
  *
  */
 function underline() {
-
    const span = document.querySelectorAll("h1 span")
 
    span.forEach(item => {
-
       item.classList.add("js-underline")
-
    })
-
 }
 
 
 /**
  *  @name arrowFade
- *
- *  @description Fades the arrow as the page scrolls down
- *  @returns {undefined} Adds or removes the "js-fade" class
+ *  @description Adds or removes the "js-fade" class
+ *  @returns {undefined}
  *
  */
 function arrowFade() {
-
    const
    wh = Math.round(window.innerHeight * 0.33),
    ws = window.scrollY
 
 	if (ws > wh && !check) {
-
       icon.classList.add("js-fade")
-      check = 1
+      check = true
 
 	} else if (ws < wh && check) {
-
       icon.classList.remove("js-fade")
-		check = 0
-
+		check = false
    }
-
 }
 
 
 /**
  *  @name validate
- *
- *  @description Validates the form
- *  @returns {undefined} Adds the "js-sub" class
+ *  @description Adds the "js-sub" class
+ *  @returns {undefined}
  *
  */
 function validate() {
-
    document.drop.classList.add("js-sub")
-
 }
 
 
@@ -122,7 +105,6 @@ function validate() {
 
 /**
  *  @name pageLoad
- *
  *  @description Listens for page to finish loading
  *  @returns {undefined} Runs several tasks when page fully loads
  *
@@ -134,9 +116,7 @@ window.addEventListener("load", function pageLoad() {
 
    // Listen: validate
    if (page("home")) {
-
       document.drop.send.addEventListener("click", validate)
-
    }
 
    // Call: navScroll
--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,7 @@
 /**
- *  @file NavJS
- *  @description Module for navigation behavior
+ *  @file Nav
+ *  @module
+ *  @description Script for navigation behavior
  */
 
 
@@ -9,13 +10,11 @@
 // Variables
 //
 
-
 const nav = document.querySelector("#nav")
 
 let
 original = window.pageYOffset,
-viewMax = 0,
-bounce = 0
+bounce
 
 
 
@@ -26,120 +25,94 @@ bounce = 0
 
 /**
  *  @name availHeight
- *
  *  @description Finds the available view height
- *  @returns {undefined} viewMax gets updated
+ *  @returns {number}
  *
  */
 function availHeight() {
-
-   const
-   full = document.documentElement.scrollHeight,
-	height = window.innerHeight
-
-   viewMax = full - height
-
+   return (
+      document.documentElement.scrollHeight - window.innerHeight
+   )
 }
 
 
 /**
  *  @name showHide
- *
- *  @description Show or Hide Nav
+ *  @description Adds or removes "js-show" class
  *  @param {number} update - Last scroll position
- *  @returns {undefined} Adds or removes "js-show" class
+ *  @returns {undefined}
  *
  */
 function showHide(update) {
 
    // Scrolling down (Hide) and bottom bounce check
    if (update > original && !bounce) {
-
       nav.classList.remove("js-show")
-      bounce = 1
+      bounce = true
 
    // Or scrolling up (Show)
    } else if (original > update) {
-
       nav.classList.add("js-show")
-      bounce = 0
-
+      bounce = false
    }
-
 }
 
 
 /**
  *  @name pageScroll
- *
  *  @description Detects page scroll position
- *  @returns {undefined} Updates page scroll position
+ *  @returns {undefined}
  *
  */
 function pageScroll() {
-
    const
    update = window.pageYOffset,
    nh = nav.clientHeight
 
 	// Check scroll position
 	if (window.pageYOffset > nh) {
-
       nav.classList.add("js-fixed")
 		nav.classList.add("js-time")
 
 		// Available scroll height check (top and bottom)
-		if (-1 < original && viewMax > update) {
+		if (-1 < original && availHeight() > update) {
 
          // Call: showHide
          showHide(update)
-
       }
 
 	// Or reset all at top
 	} else if (0 === window.pageYOffset) {
-
       nav.classList.remove("js-fixed")
       nav.classList.remove("js-show")
 
    // Or remove transition in Nav height area
 	} else {
-
       nav.classList.remove("js-time")
-
    }
 
 	// Update scroll position
 	original = update
-
 }
 
 
 
 //
-// Export
+// Exports
 //
 
 
 /**
  *  @name navScroll
- *
  *  @description Controls the
  *  @returns {undefined}
  *
  */
 export default function navScroll() {
 
-   // Listen: availHeight
-   window.addEventListener("resize", availHeight)
-
    // Listen: pageScroll
    window.addEventListener("scroll", pageScroll)
 
    // Call: pageScroll
    pageScroll()
-
-   // Call: availHeight
-   availHeight()
-
 }
