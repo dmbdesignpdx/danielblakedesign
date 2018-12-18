71a89e6699e5b2a6697bc1400a9a2bb4e4a1fada
dmbdesignpdx <daniel@danielblake.design>
2018-12-18 15:17:50 -0800

    updates assets with service workers

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,7 @@
+"use strict"
+
 const
-name = "daniel-blake-v1",
+name = "daniel-blake-v2",
 assets = [
    "/",
    "/flexr/",
@@ -10,54 +12,54 @@ assets = [
    "/css/master.min.css",
    "/dist/main.min.js",
    "/manifest.json",
+   "/fonts/montserrat-900.woff",
    "/img/icons.svg",
    "/img/favicon.svg",
    "/img/favicon-96x96.png",
    "/img/favicon-192x192.png",
    "/img/android-chrome-192x192.png",
    "/img/pinned-tab.svg",
-   "/img/work-md.webp",
+   "/img/work-fl.webp",
    "/img/work-sm.webp",
-   "/img/work-xs.webp",
-   "/img/flexr/flexr-1-md.webp",
-   "/img/flexr/flexr-1-sm.webp",
-   "/img/flexr/flexr-1-xs.webp",
-   "/img/flexr/flexr-2-md.webp",
-   "/img/flexr/flexr-2-sm.webp",
-   "/img/flexr/flexr-2-xs.webp",
-   "/img/flexr/flexr-3-md.webp",
-   "/img/flexr/flexr-3-sm.webp",
-   "/img/flexr/flexr-3-xs.webp",
-   "/img/leadshub/leadshub-1-md.webp",
-   "/img/leadshub/leadshub-1-sm.webp",
-   "/img/leadshub/leadshub-1-xs.webp",
-   "/img/leadshub/leadshub-2-md.webp",
-   "/img/leadshub/leadshub-2-sm.webp",
-   "/img/leadshub/leadshub-2-xs.webp",
-   "/img/leadshub/leadshub-3-md.webp",
-   "/img/leadshub/leadshub-3-sm.webp",
-   "/img/leadshub/leadshub-3-xs.webp",
-   "/img/turntable/turntable-1-md.webp",
+   "/img/work-lg.webp",
+   "/img/work-xl.webp",
+   "/img/turntable/turntable-1-fl.webp",
    "/img/turntable/turntable-1-sm.webp",
-   "/img/turntable/turntable-1-xs.webp",
-   "/img/turntable/turntable-2-md.webp",
+   "/img/turntable/turntable-1-lg.webp",
+   "/img/turntable/turntable-1-xl.webp",
+   "/img/turntable/turntable-2-fl.webp",
    "/img/turntable/turntable-2-sm.webp",
-   "/img/turntable/turntable-2-xs.webp",
-   "/img/turntable/turntable-3-md.webp",
+   "/img/turntable/turntable-2-lg.webp",
+   "/img/turntable/turntable-2-xl.webp",
+   "/img/turntable/turntable-3-fl.webp",
    "/img/turntable/turntable-3-sm.webp",
-   "/img/turntable/turntable-3-xs.webp",
+   "/img/turntable/turntable-3-lg.webp",
+   "/img/turntable/turntable-3-xl.webp",
+   "/img/flexr/flexr-1-fl.webp",
+   "/img/flexr/flexr-1-sm.webp",
+   "/img/flexr/flexr-1-lg.webp",
+   "/img/flexr/flexr-1-xl.webp",
+   "/img/flexr/flexr-2-fl.webp",
+   "/img/flexr/flexr-2-sm.webp",
+   "/img/flexr/flexr-2-lg.webp",
+   "/img/flexr/flexr-2-xl.webp",
+   "/img/flexr/flexr-3-fl.webp",
+   "/img/flexr/flexr-3-sm.webp",
+   "/img/flexr/flexr-3-lg.webp",
+   "/img/flexr/flexr-3-xl.webp",
+   "/img/leadshub/leadshub-1-fl.webp",
+   "/img/leadshub/leadshub-1-sm.webp",
+   "/img/leadshub/leadshub-1-lg.webp",
+   "/img/leadshub/leadshub-1-xl.webp",
+   "/img/leadshub/leadshub-2-fl.webp",
+   "/img/leadshub/leadshub-2-sm.webp",
+   "/img/leadshub/leadshub-2-lg.webp",
+   "/img/leadshub/leadshub-2-xl.webp",
+   "/img/leadshub/leadshub-3-fl.webp",
+   "/img/leadshub/leadshub-3-sm.webp",
+   "/img/leadshub/leadshub-3-lg.webp",
+   "/img/leadshub/leadshub-3-xl.webp",
 ]
-
-
-function autoCache(request) {
-   fetch(request).then(response => {
-      return caches.open(name).then(cache => {
-         cache.put(request.url, response.clone())
-   
-         return response
-      })
-   })
-}
 
 
 self.addEventListener("install", event => {
@@ -75,7 +77,7 @@ self.addEventListener("fetch", event => {
 
    event.respondWith(
       caches.match(request).then(response => {
-         return response || autoCache(request)
+         return response || fetch(request)
       })
    )
 })
