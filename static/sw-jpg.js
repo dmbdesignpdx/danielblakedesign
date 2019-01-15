67ebfeaed657e67cb36d59ef1f4c8d48c5b6201f
dmbdesignpdx <daniel@danielblake.design>
2019-01-15 11:55:32 -0800

    removes workers

--- static/sw-jpg.js
+++ static/sw-jpg.js
@@ -1,99 +1,31 @@
 "use strict"
 
-const
-name = "daniel-blake-jpg-v4",
-assets = [
-   "/",
-   "/flexr/",
-   "/turntable/",
-   "/leadshub/",
-   "/favicon.ico",
-   "/apple-touch-icon.png",
-   "/css/master.min.css",
-   "/dist/main.min.js",
-   "/manifest.json",
-   "/fonts/montserrat-900.woff",
-   "/img/icons.svg",
-   "/img/favicon.svg",
-   "/img/favicon-96x96.png",
-   "/img/favicon-192x192.png",
-   "/img/android-chrome-192x192.png",
-   "/img/pinned-tab.svg",
-   "/img/work-sm.jpg",
-   "/img/work-lg.jpg",
-   "/img/work-xl.jpg",
-   "/img/work-fl.jpg",
-   "/img/flexr/flexr-1-sm.jpg",
-   "/img/flexr/flexr-1-lg.jpg",
-   "/img/flexr/flexr-1-xl.jpg",
-   "/img/flexr/flexr-1-fl.jpg",
-   "/img/flexr/flexr-2-sm.jpg",
-   "/img/flexr/flexr-2-lg.jpg",
-   "/img/flexr/flexr-2-xl.jpg",
-   "/img/flexr/flexr-2-fl.jpg",
-   "/img/flexr/flexr-3-sm.jpg",
-   "/img/flexr/flexr-3-lg.jpg",
-   "/img/flexr/flexr-3-xl.jpg",
-   "/img/flexr/flexr-3-fl.jpg",
-   "/img/leadshub/leadshub-1-sm.jpg",
-   "/img/leadshub/leadshub-1-lg.jpg",
-   "/img/leadshub/leadshub-1-xl.jpg",
-   "/img/leadshub/leadshub-1-fl.jpg",
-   "/img/leadshub/leadshub-2-sm.jpg",
-   "/img/leadshub/leadshub-2-lg.jpg",
-   "/img/leadshub/leadshub-2-xl.jpg",
-   "/img/leadshub/leadshub-2-fl.jpg",
-   "/img/leadshub/leadshub-3-sm.jpg",
-   "/img/leadshub/leadshub-3-lg.jpg",
-   "/img/leadshub/leadshub-3-xl.jpg",
-   "/img/leadshub/leadshub-3-fl.jpg",
-   "/img/turntable/turntable-1-sm.jpg",
-   "/img/turntable/turntable-1-lg.jpg",
-   "/img/turntable/turntable-1-xl.jpg",
-   "/img/turntable/turntable-1-fl.jpg",
-   "/img/turntable/turntable-2-sm.jpg",
-   "/img/turntable/turntable-2-lg.jpg",
-   "/img/turntable/turntable-2-xl.jpg",
-   "/img/turntable/turntable-2-fl.jpg",
-   "/img/turntable/turntable-3-sm.jpg",
-   "/img/turntable/turntable-3-lg.jpg",
-   "/img/turntable/turntable-3-xl.jpg",
-   "/img/turntable/turntable-3-fl.jpg",
-]
+// Unregisters service worker and deletes its cache for danielblake.design
 
 
-self.addEventListener("install", event => {
-   self.skipWaiting()
-
-   event.waitUntil(
-      caches.open(name).then(cache => {
-         return cache.addAll(assets)
-      })
-   )
-})
-
-self.addEventListener("fetch", event => {
-   const request = event.request
-
-   event.respondWith(
-      caches.match(request).then(response => {
-         return response || fetch(request)
-      })
-   )
+self.addEventListener('install', () => {
+  self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
-   const keep = [name]
+  
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then(clients => {
+      clients.forEach(client => client.navigate(client.url))
+    })
+
+    const keep = ["remove"]
  
-   event.waitUntil(
-      caches.keys().then(keys => {
-         return Promise.all(
-            keys.map(key => {
-               if (!keep.includes(key)) {
-                  return caches.delete(key)
-               }
-            })
-         )
-      })
-   )
+    event.waitUntil(
+       caches.keys().then(keys => {
+          return Promise.all(
+             keys.map(key => {
+                if (!keep.includes(key)) {
+                   return caches.delete(key)
+                }
+             })
+          )
+       })
+    )
 })
--- static/sw.js
+++ static/sw.js
@@ -1,99 +1,31 @@
 "use strict"
 
-const
-name = "daniel-blake-v4",
-assets = [
-   "/",
-   "/flexr/",
-   "/turntable/",
-   "/leadshub/",
-   "/favicon.ico",
-   "/apple-touch-icon.png",
-   "/css/master.min.css",
-   "/dist/main.min.js",
-   "/manifest.json",
-   "/fonts/montserrat-900.woff",
-   "/img/icons.svg",
-   "/img/favicon.svg",
-   "/img/favicon-96x96.png",
-   "/img/favicon-192x192.png",
-   "/img/android-chrome-192x192.png",
-   "/img/pinned-tab.svg",
-   "/img/work-fl.webp",
-   "/img/work-sm.webp",
-   "/img/work-lg.webp",
-   "/img/work-xl.webp",
-   "/img/turntable/turntable-1-fl.webp",
-   "/img/turntable/turntable-1-sm.webp",
-   "/img/turntable/turntable-1-lg.webp",
-   "/img/turntable/turntable-1-xl.webp",
-   "/img/turntable/turntable-2-fl.webp",
-   "/img/turntable/turntable-2-sm.webp",
-   "/img/turntable/turntable-2-lg.webp",
-   "/img/turntable/turntable-2-xl.webp",
-   "/img/turntable/turntable-3-fl.webp",
-   "/img/turntable/turntable-3-sm.webp",
-   "/img/turntable/turntable-3-lg.webp",
-   "/img/turntable/turntable-3-xl.webp",
-   "/img/flexr/flexr-1-fl.webp",
-   "/img/flexr/flexr-1-sm.webp",
-   "/img/flexr/flexr-1-lg.webp",
-   "/img/flexr/flexr-1-xl.webp",
-   "/img/flexr/flexr-2-fl.webp",
-   "/img/flexr/flexr-2-sm.webp",
-   "/img/flexr/flexr-2-lg.webp",
-   "/img/flexr/flexr-2-xl.webp",
-   "/img/flexr/flexr-3-fl.webp",
-   "/img/flexr/flexr-3-sm.webp",
-   "/img/flexr/flexr-3-lg.webp",
-   "/img/flexr/flexr-3-xl.webp",
-   "/img/leadshub/leadshub-1-fl.webp",
-   "/img/leadshub/leadshub-1-sm.webp",
-   "/img/leadshub/leadshub-1-lg.webp",
-   "/img/leadshub/leadshub-1-xl.webp",
-   "/img/leadshub/leadshub-2-fl.webp",
-   "/img/leadshub/leadshub-2-sm.webp",
-   "/img/leadshub/leadshub-2-lg.webp",
-   "/img/leadshub/leadshub-2-xl.webp",
-   "/img/leadshub/leadshub-3-fl.webp",
-   "/img/leadshub/leadshub-3-sm.webp",
-   "/img/leadshub/leadshub-3-lg.webp",
-   "/img/leadshub/leadshub-3-xl.webp",
-]
+// Unregisters service worker and deletes its cache for danielblake.design
 
 
-self.addEventListener("install", event => {
-   self.skipWaiting()
-
-   event.waitUntil(
-      caches.open(name).then(cache => {
-         return cache.addAll(assets)
-      })
-   )
-})
-
-self.addEventListener("fetch", event => {
-   const request = event.request
-
-   event.respondWith(
-      caches.match(request).then(response => {
-         return response || fetch(request)
-      })
-   )
+self.addEventListener('install', () => {
+  self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
-   const keep = [name]
+  
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then(clients => {
+      clients.forEach(client => client.navigate(client.url))
+    })
+
+    const keep = ["remove"]
  
-   event.waitUntil(
-      caches.keys().then(keys => {
-         return Promise.all(
-            keys.map(key => {
-               if (!keep.includes(key)) {
-                  return caches.delete(key)
-               }
-            })
-         )
-      })
-   )
+    event.waitUntil(
+       caches.keys().then(keys => {
+          return Promise.all(
+             keys.map(key => {
+                if (!keep.includes(key)) {
+                   return caches.delete(key)
+                }
+             })
+          )
+       })
+    )
 })
